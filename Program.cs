c11ce61cac3d1a3e18ab23b6ed9ef72f19819c16
Spandiar <spandiarsantal@gmail.com>
2024-04-02 01:32:57 +0330

    Made the project and added the initial scripts.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Console.WriteLine("Hello, World!");
+// int i = int.Parse(Console.ReadLine());
+
+using System.Diagnostics;
+
+Console.WriteLine("Hello dude, please write what you want to do: \n \n1. To print the list of open programs.\n2. To execute a program.\n3. To kill a program");
+
+string userIn = Console.ReadLine();
+
