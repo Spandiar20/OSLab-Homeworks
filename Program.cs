9443f01d1ea7157a135f445d4e443588d13fa058
Spandiar <spandiarsantal@gmail.com>
2024-04-02 02:27:42 +0330

    Made the code functional and finalized the stuff.

--- Program.cs
+++ Program.cs
@@ -5,39 +5,63 @@ using System.Diagnostics;
 
 Console.WriteLine("Hello dude, please write what you want to do: \n \n1. To print the list of open programs.\n2. To execute a program.\n3. To kill a program");
 
-string userIn = Console.ReadLine();
+while(true)
+{
+    string userIn = Console.ReadLine();
 
-switch(userIn) {
-    
-    case "2":
-        Console.WriteLine("A'ight, type the name of the Program do you want me to execute: ");
-        userIn = Console.ReadLine();
-        Process.Start(userIn);
-        break;
+    switch (userIn)
+    {
 
-    case "3":
-        Process[] processes = Process.GetProcesses();
-        Console.WriteLine("Here are running programs:");
-        foreach (Process process in processes){
-            Console.WriteLine($"{process.Id}   {process.ProcessName}");
+        case "2":
+            option2();
+            break;
+
+        case "3":
+            option3();
+            break;
+
+        case "1":
+            option1();
+            break;
+
+        default:
+            Console.WriteLine("\nProgram closed with a not valid input.");
+            return;
+
+    }
+}
+
+static void option1() 
+{
+    Process[] procs = Process.GetProcesses();
+    Console.WriteLine("Here is a list of opened and executing processes in your system:");
+    foreach (Process process in procs)
+    {
+        Console.WriteLine($"{process.Id}   {process.ProcessName}");
+    }
+}
+
+static void option2() {
+    Console.WriteLine("A'ight, type the name of the Program do you want me to execute: ");
+    string userInput = Console.ReadLine();
+    Process.Start(userInput);
+}
+
+static void option3() {
+    Process[] processes = Process.GetProcesses();
+    Console.WriteLine("Here are running programs:");
+    foreach (Process process in processes)
+    {
+        Console.WriteLine($"{process.Id}   {process.ProcessName}");
+    }
+    Console.WriteLine("Write down the ID of the program do you want to kill:");
+    int userIndex = int.Parse(Console.ReadLine());
+
+    foreach (Process process in processes){
+        if (userIndex == process.Id)
+        {
+            process.Kill();
+            Console.WriteLine($"Process with the Id of {userIndex} has been closed!");
         }
-        Console.WriteLine("Write down the ID of the program do you want to kill:");
-        int userIndex = int.Parse(Console.ReadLine());
-        
-        foreach(Process process in processes) {
-            if (userIndex == process.Id) {
-                process.Kill();
-                Console.WriteLine($"Process with the Id of {userIndex} has been closed!");
-            }
-        }
-        break;
-
-    case "1":
-        Process[] procs = Process.GetProcesses();
-        Console.WriteLine("Here is a list of opened and executing processes in your system:");
-        foreach(Process process in procs) {
-            Console.WriteLine($"{process.Id}   {process.ProcessName}");
-        }
-        break;
-
+    }
 }
--- obj/Debug/net8.0/OSLabS1.AssemblyInfo.cs
+++ obj/Debug/net8.0/OSLabS1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OSLabS1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3239fbef9ef82281089c7202e636dc1c94665481")]
 [assembly: System.Reflection.AssemblyProductAttribute("OSLabS1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OSLabS1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
